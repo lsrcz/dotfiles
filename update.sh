ce57f410c9b7832e650921624773fae564f879c2
Sirui Lu <siruilu@cs.washington.edu>
2024-02-04 00:19:31 -0800

    :hammer: Fix linux script

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    Linux*)     nixos-rebuild switch --flake .#$(hostname);;
+    Linux*)     sudo nixos-rebuild switch --flake .#$(hostname);;
     Darwin*)    darwin-rebuild switch --flake .#$(hostname);;
     *)          echo "Unknown system ${unameOut}" && exit -1;; 
 esac
