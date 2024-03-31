9b769fe7eb0ef6f5e3a372be4821903da3631a13
Sirui Lu <siruilu@cs.washington.edu>
2024-03-30 19:01:20 -0700

    :lock: Temporarily use staging-next channel to avoid the recent CVE-2024-3094 exploit in xz-5.6.x

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
+      url = "github:NixOS/nixpkgs/staging-next";
     };
     home-manager = {
       url = "github:nix-community/home-manager/master";
