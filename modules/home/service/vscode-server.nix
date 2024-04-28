480c68e30f62c24bc57823e676c07ba0fe41c851
Sirui Lu <siruilu@cs.washington.edu>
2024-04-28 15:13:29 -0700

    :arrow_up: Upgrade flake.lock and vscode-server

--- modules/home/service/vscode-server.nix
+++ modules/home/service/vscode-server.nix
@@ -2,7 +2,7 @@
   imports = [
     "${fetchTarball {
         url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
-        sha256 = "sha256:1mrc6a1qjixaqkv1zqphgnjjcz9jpsdfs1vq45l1pszs9lbiqfvd";
+        sha256 = "sha256:1rq8mrlmbzpcbv9ys0x88alw30ks70jlmvnfr2j8v830yy5wvw7h";
     }}/modules/vscode-server/home.nix"
   ];
 }
