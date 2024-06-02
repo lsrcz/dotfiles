1c1ccb00e658dfcb10c463a5afd29530fe8ce885
Sirui Lu <siruilu@cs.washington.edu>
2024-06-01 22:33:45 -0700

    :sparkles: Add more hosts

--- home/siruilu/zeus/default.nix
+++ home/siruilu/zeus/default.nix
@@ -28,5 +28,18 @@
       hostname = "172.234.236.43";
       user = "siruilu";
     };
+    ptc = {
+      identityFile = "/home/siruilu/.ssh/id_ed25519_keyless";
+      port = 22;
+      hostname = "ptc.cs.washington.edu";
+      user = "siruilu";
+      proxyJump = "tricycle";
+    };
+    bananapi = {
+      identityFile = "/home/siruilu/.ssh/id_ed25519_keyless";
+      port = 22;
+      hostname = "192.168.0.63";
+      user = "siruilu";
+    };
   };
 }
