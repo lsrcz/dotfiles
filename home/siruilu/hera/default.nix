62cb4ae2f537bb29aa212cc40fc04267c1d14494
Sirui Lu <siruilu@cs.washington.edu>
2024-06-03 20:44:37 -0700

    :sparkles: Add banana-pi

--- home/siruilu/hera/default.nix
+++ home/siruilu/hera/default.nix
@@ -35,12 +35,30 @@
       hostname = "recycle.cs.washington.edu";
       user = "siruilu";
     };
+    tricycle = {
+      identityFile = "/Users/siruilu/.ssh/id_ed25519";
+      port = 22;
+      hostname = "tricycle.cs.washington.edu";
+      user = "siruilu";
+    };
     ptc-jump = {
       identityFile = "/Users/siruilu/.ssh/id_ed25519";
       port = 22;
       hostname = "ptc.cs.washington.edu";
       user = "siruilu";
-      proxyJump = "recycle";
+      proxyJump = "tricycle";
+    };
+    bananapi-local = {
+      identityFile = "/Users/siruilu/.ssh/id_ed25519";
+      port = 22;
+      hostname = "192.168.0.63";
+      user = "siruilu";
+    };
+    bananapi = {
+      identityFile = "/Users/siruilu/.ssh/id_ed25519";
+      port = 28095;
+      hostname = "172.234.236.43";
+      user = "siruilu";
     };
   };
   programs.ssh.extraConfig = ''
@@ -48,3 +66,4 @@
     IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
   '';
 }
+
